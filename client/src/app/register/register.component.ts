f66404935eacde67196c59fd2902210b1c912f9f
Jovica <jovica.misirlic@gmail.com>
2021-05-11 00:41:54 +0200

    Added toastr service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -22,8 +23,10 @@ export class RegisterComponent implements OnInit {
       this.cancel();
     }, error => {
       console.log(error);
+      this.toast.error(error.error);
     })
   }
+  
 
   cancel() {
     this.cancelRegister.emit(false);
